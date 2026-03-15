269066025f73efec3b0b58a98037e0f6e54486ed
VladimirBelyaev01 <volodya.belyaev.34@gmail.com>
2026-03-15 17:24:50 +0300

    продолжение с 11 шага

---
+++ app/src/main/java/com/belyaev/selivanov/student_material_design_bel_sel/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.belyaev.selivanov.student_material_design_bel_sel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.belyaev.selivanov.student_material_design_bel_sel.data.Student
+import com.belyaev.selivanov.student_material_design_bel_sel.ui.theme.Student_Material_Design_bel_selTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.belyaev.selivanov.student_material_design_bel_sel.data.students
+import kotlinx.serialization.descriptors.SerialKind
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Student_Material_Design_bel_selTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentApp()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun StudentIcon(
+    @DrawableRes studentIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop,
+        painter = painterResource(studentIcon),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun StudentInformation(
+    @StringRes studentName: Int,
+    studentAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(studentName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            text = stringResource(R.string.years_old, studentAge),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+@Composable
+fun StudentItem(
+    student: Student,
+    modifier: Modifier = Modifier
+)
+ {
+    Card(modifier = modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            StudentIcon(student.imageResourceId)
+            StudentInformation(student.name, student.age)
+        }
+    }
+}
+@Composable
+ fun StudentApp() {
+    Scaffold (
+        topBar =  {
+            StudentTopAppBar()
+        }
+    ){ it ->
+        LazyColumn(contentPadding = it) {
+            items(students) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun StudentPreview() {
+    Student_Material_Design_bel_selTheme(darkTheme = false) {
+        StudentApp()
+    }
+}
+
+@Preview
+@Composable
+fun StudentDarkThemePreview(){
+    Student_Material_Design_bel_selTheme(darkTheme = true) {
+        StudentApp()
+    }
+}
